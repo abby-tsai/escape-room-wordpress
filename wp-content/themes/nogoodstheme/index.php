3c74d5e563315a25caaedaec8367d8cc1bd5ec93
abby <diya811125@gmail.com>
2023-12-25 20:47:09 +0800

    更改圖片路徑和x軸跑板問題

--- wp-content/themes/nogoodstheme/index.php
+++ wp-content/themes/nogoodstheme/index.php
@@ -4,16 +4,17 @@
     <?php get_template_part('company'); ?>
   </section>
   <section class="my-5 py-5 bg-light">
-    <div>
+    <div class="container">
       <?php get_template_part('about'); ?>
     </div>
   </section>
   <section class="mb-5">
-    <?php get_template_part('assess'); ?>
+    <div class="container">
+      <?php get_template_part('assess'); ?>
+    </div>
   </section>
   <section class="bg-light">
     <?php get_template_part('join'); ?>
   </section>
 </main>
 <?php get_footer(); ?>
-  
